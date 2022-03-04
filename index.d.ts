cbb5ca2cbd758d17784d1774477e2b274740f463
supervons <supervons@sina.com>
2022-03-04 16:13:49 +0800

    [feature] Support Typescript.

---
+++ index.d.ts
@@ -0,0 +1,14 @@
+declare function RNEChartsPro(props: {
+  option: object;
+  height?: number;
+  themeName?: string;
+  customMapData?: object;
+  webViewSettings?: any;
+  backgroundColor?: string;
+  formatterVariable?: object;
+  legendSelectChanged?(result: string);
+  onDataZoom?(result: string);
+  onPress?(result: string);
+}): JSX.Element;
+
+export default RNEChartsPro;
